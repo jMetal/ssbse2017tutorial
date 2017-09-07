cf16cd0b1c862fff4b4925b33c6aaec22fb08935
Antonio J. Nebro <antonio@lcc.uma.es>
2017-09-07 17:10:59 +0200

    Adding resources

---
+++ code/src/main/java/ssbse2017/runner/NSGAIIRunnerWithChart.java
@@ -0,0 +1,151 @@
+package ssbse2017.runner;
+
+import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAIIBuilder;
+import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAIIMeasures;
+import org.uma.jmetal.measure.MeasureListener;
+import org.uma.jmetal.measure.MeasureManager;
+import org.uma.jmetal.measure.impl.BasicMeasure;
+import org.uma.jmetal.measure.impl.CountingMeasure;
+import org.uma.jmetal.operator.CrossoverOperator;
+import org.uma.jmetal.operator.MutationOperator;
+import org.uma.jmetal.operator.SelectionOperator;
+import org.uma.jmetal.operator.impl.crossover.SinglePointCrossover;
+import org.uma.jmetal.operator.impl.mutation.BitFlipMutation;
+import org.uma.jmetal.operator.impl.selection.BinaryTournamentSelection;
+import org.uma.jmetal.solution.BinarySolution;
+import org.uma.jmetal.util.AlgorithmRunner;
+import org.uma.jmetal.util.JMetalException;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.comparator.RankingAndCrowdingDistanceComparator;
+import org.uma.jmetal.util.front.Front;
+import org.uma.jmetal.util.front.imp.ArrayFront;
+import org.uma.jmetal.util.point.impl.ArrayPoint;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ssbse2017.util.ChartContainer;
+import ssbse2017.NRPProblem;
+
+/**
+ * Class to configure and run the NSGA-II algorithm
+ *
+ * @author Antonio J. Nebro <antonio@lcc.uma.es>
+ */
+public class NSGAIIRunnerWithChart {
+  /**
+   * @param args Command line arguments.
+   * @throws FileNotFoundException Invoking command: java org.uma.jmetal.runner.multiobjective.NSGAIIRunner
+   *                               problemName [referenceFront]
+   */
+  public static void main(String[] args) throws JMetalException, FileNotFoundException {
+    NSGAIIMeasures<BinarySolution> algorithm;
+    CrossoverOperator<BinarySolution> crossover;
+    MutationOperator<BinarySolution> mutation;
+    SelectionOperator<List<BinarySolution>, BinarySolution> selection;
+
+    int numberOfClients = 20;
+    int numberOfRequirements = 30;
+    int highestClientSatisfactionValue = 10;
+    int highestRequirementCost = 200;
+    int highestImportanceValue = 100;
+    int randomSeed = 1;
+    NRPProblem problem = new NRPProblem(
+            numberOfClients,
+            numberOfRequirements,
+            highestClientSatisfactionValue,
+            highestRequirementCost,
+            highestImportanceValue,
+            randomSeed);
+
+    double crossoverProbability = 0.9;
+    crossover = new SinglePointCrossover(crossoverProbability);
+
+    double mutationProbability = 1.0 / problem.getTotalNumberOfBits();
+    mutation = new BitFlipMutation(mutationProbability);
+
+    selection = new BinaryTournamentSelection<BinarySolution>(
+            new RankingAndCrowdingDistanceComparator<BinarySolution>());
+
+    algorithm = (NSGAIIMeasures<BinarySolution>) new NSGAIIBuilder<BinarySolution>(problem, crossover, mutation)
+            .setSelectionOperator(selection)
+            .setMaxEvaluations(25000)
+            .setPopulationSize(100)
+            .setVariant(NSGAIIBuilder.NSGAIIVariant.Measures)
+            .build();
+
+
+
+    Front referenceFront = new ArrayFront(1, 2) ;
+    double[] referencePoint = {3600, 0} ;
+    referenceFront.setPoint(0, new ArrayPoint(referencePoint));
+    ((NSGAIIMeasures<BinarySolution>) algorithm).setReferenceFront(new ArrayFront(referenceFront));
+
+            /* Measure management */
+    MeasureManager measureManager = ((NSGAIIMeasures<BinarySolution>) algorithm).getMeasureManager();
+
+    BasicMeasure<List<BinarySolution>> solutionListMeasure = (BasicMeasure<List<BinarySolution>>) measureManager
+            .<List<BinarySolution>>getPushMeasure("currentPopulation");
+    CountingMeasure iterationMeasure = (CountingMeasure) measureManager.<Long>getPushMeasure("currentEvaluation");
+
+    ChartContainer chart = new ChartContainer(algorithm.getName(), 100);
+    chart.setFrontChart(0, 1, null);
+    //chart.addIndicatorChart("Hypervolume");
+    chart.initChart();
+
+    solutionListMeasure.register(new ChartListener(chart));
+    iterationMeasure.register(new IterationListener(chart));
+
+        /* End of measure management */
+
+    AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
+            .execute();
+
+    List<BinarySolution> population = algorithm.getResult();
+    long computingTime = algorithmRunner.getComputingTime();
+
+    JMetalLogger.logger.info("Total execution time: " + computingTime + "ms");
+
+  }
+
+  private static class ChartListener implements MeasureListener<List<BinarySolution>> {
+    private ChartContainer chart;
+    private int iteration = 0;
+
+    public ChartListener(ChartContainer chart) {
+      this.chart = chart;
+      this.chart.getFrontChart().setTitle("Iteration: " + this.iteration);
+    }
+
+    private void refreshChart(List<BinarySolution> solutionList) {
+      if (this.chart != null) {
+        iteration++;
+        this.chart.getFrontChart().setTitle("Iteration: " + this.iteration);
+        this.chart.updateFrontCharts(solutionList);
+        this.chart.refreshCharts();
+      }
+    }
+
+    @Override
+    synchronized public void measureGenerated(List<BinarySolution> solutions) {
+      refreshChart(solutions);
+    }
+  }
+
+  private static class IterationListener implements MeasureListener<Long> {
+    ChartContainer chart;
+
+    public IterationListener(ChartContainer chart) {
+      this.chart = chart;
+      this.chart.getFrontChart().setTitle("Iteration: " + 0);
+    }
+
+    @Override
+    synchronized public void measureGenerated(Long iteration) {
+      if (this.chart != null) {
+        this.chart.getFrontChart().setTitle("Iteration: " + iteration);
+      }
+    }
+  }
+
+}
